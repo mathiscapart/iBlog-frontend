7c0a2a93c4a5c103f80f556b84d29b617f4ba7b3
mathiscapart <mathiscapart@icloud.com>
2025-05-13 15:17:19 +0200

    add gitflow test and specifiq lint

---
+++ tests/math.test.tsx
@@ -0,0 +1,12 @@
+import { describe, test, expect } from "vitest"
+
+describe("Mathematic Operations", () => {
+    test("should add 2 numbers", () => {
+        expect(1+1).toBe(2);
+    });
+
+    test("should return the same numbers", () => {
+        expect(1).toBe(1)
+    })
+
+})
